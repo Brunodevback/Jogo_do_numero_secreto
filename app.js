4c0c70b1cbac317bb3be14a54964670aa733d0b9
Bruno Marinho <brbruno1010@gmail.com>
2025-01-20 21:30:45 -0300

    Número limite para 100

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listadeNumerosSorteados = [ ];
-let numeroLimite = 10
+let numeroLimite = 100
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1
 
@@ -17,7 +17,7 @@ function exibirTextonaTela(tag, texto){
 }
 function textoInicial(){
     exibirTextonaTela("h1", "Jogo do número secreto");
-    exibirTextonaTela("p", "Escolha um número entre 1 e 10");
+    exibirTextonaTela("p", "Escolha um número entre 1 e 100");
 }
 textoInicial();
 
